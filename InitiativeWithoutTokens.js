9607b78ae96754f42c56d05d89fb09d212d34e42
NinhDo <doquininh@gmail.com>
2015-06-13 18:37:06 +0200

    Update InitiativeWithoutTokens.js

--- InitiativeWithoutTokens.js
+++ InitiativeWithoutTokens.js
@@ -1,11 +1,17 @@
 /*
+Github:     https://github.com/NinhDo/Roll20/blob/master/InitiativeWithoutTokens.js
+By:         Ninh Do
+Contact:    https://app.roll20.net/users/190323/ninh
+
+#########################
 Initiative without tokens
-by Ninh Do, https://app.roll20.net/users/190323/ninh
+#########################
 
+Use by typing "!initiative" in chat, followed by the modifier. No modifier is counted as a 0.
+I reccomend making a macro prompting the user to add their modifier:
+    !initiative ?{Initiative(DEX|0)}
 
-
-Use by typing "!initiative" in chat, followed by the modifier. No modifier is counted as a 0
-example:
+Examples on how this script works:
 !initiative -> rolls 1d20 and adds it to the turn order
 !initiative 4 -> rolls 1d20+4 and adds it to the turn order
 */
@@ -16,6 +22,7 @@ on("chat:message", function(msg) {
     var roller = msg.who;
     var splitMsg = msg.content.split(" ");
     var command = splitMsg[0];
+    // Modifier is 0 if no number is added after the command
     var modifier = (splitMsg[1] != null) ? splitMsg[1] : 0;
     var string = "[[d20+" + modifier + "]]";
     if(command == "!initiative") {
@@ -29,7 +36,11 @@ on("chat:message", function(msg) {
                 custom: roller
             });
             Campaign().set("turnorder", JSON.stringify(turnorder));
-            sendChat(roller, "/me rolling for initiative: " +  rollResults.inlinerolls[1].expression + " = " + rollValue + " (" + rollResults.inlinerolls[1].results.rolls[0].results[0].v + rollResults.inlinerolls[1].results.rolls[1].expr + ")");
+            // Writes the result to chat
+            sendChat(roller, "/me rolling for initiative: " 
+                + rollResults.inlinerolls[1].expression + " = " 
+                + rollValue + " (" + rollResults.inlinerolls[1].results.rolls[0].results[0].v 
+                + rollResults.inlinerolls[1].results.rolls[1].expr + ")");
         })
     }
 });
