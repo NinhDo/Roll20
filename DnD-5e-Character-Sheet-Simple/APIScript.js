78ff05b7df83adbf77854f452535399572ea4648
NinhDo <doquininh@gmail.com>
2015-07-04 03:48:34 +0200

    Update APIScript.js

--- DnD-5e-Character-Sheet-Simple/APIScript.js
+++ DnD-5e-Character-Sheet-Simple/APIScript.js
@@ -1,3 +1,24 @@
+on("add:character", function(obj) {
+    createObj("attribute", {
+        name: "Level",
+        current: 0,
+        max: 20,
+        characterid: obj.id,
+    });
+    createObj("attribute", {
+        name: "NextLevel",
+        current: 300,
+        max: "",
+        characterid: obj.id,
+    });
+    createObj("attribute", {
+        name: "Experience",
+        current: 0,
+        max: "",
+        characterid: obj.id,
+    });
+});
+
 on("change:attribute", function(obj) {
     if(obj.get("name") == "Experience" && isNumeric(obj.get("current"))) {
         CalculateEXP(obj);
@@ -21,30 +42,18 @@ function CalculateEXP(obj) {
         _characterid: charID,
         name: 'NextLevel',
     })[0];
-    if(!Level) {
-        Level = createObj('attribute', {
-            characterid: charID,
-            name: "Level",
-            current: "1",
-        });
-    }
-    if(!NextLevel) {
-        NextLevel = createObj('attribute', {
-            characterid: charID,
-            name: "NextLevel",
-            current: "300",
-        });
-    }
     var EXPArray = [300, 900, 2700, 6500, 14000, 23000, 34000, 48000, 64000, 85000, 100000, 120000, 140000, 165000, 195000, 225000, 265000, 305000, 355000];
     for(var i = 0; i < EXPArray.length; i++) {
         if(EXP < EXPArray[i]) {
-            NextLevel.set("current", EXPArray[i]);
-            Level.set("current", i+1);
+            NextLevel.set("current", EXPArray[i].toString());
+            Level.set("current", (i+1).toString());
             break;
         } else if(EXP >= 355000) {
             NextLevel.set("current", "N/A");
-            Level.set("current", 20);
+            Level.set("current", "20");
             break;
         }
     }
+    log(Level);
+    log(NextLevel);
 }
