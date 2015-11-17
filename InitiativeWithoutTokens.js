b2fd701a10027eab5566a1750b3d5a954994eacf
Ninh <doquininh@gmail.com>
2015-11-17 14:08:36 +0100

    Version 1.2
    VERSION 1.2 CHANGES
    You can now add text at THE END of the roll, and that name will be added to the turn order
    For example:
    /r d20+5 !initiative John
    Will add "John" in the turn order

--- InitiativeWithoutTokens.js
+++ InitiativeWithoutTokens.js
@@ -2,7 +2,7 @@
 Github:     https://github.com/NinhDo/Roll20/blob/master/InitiativeWithoutTokens.js
 By:         Ninh Do
 Contact:    https://app.roll20.net/users/190323/ninh
-Version:    1.1
+Version:    1.2
 
 #########################
 Initiative without tokens
@@ -19,11 +19,20 @@ I suggest making a macro querying the player for a modifier.
 /roll d20+?{Initiative(DEX)|0} !initiative
 
 Does not work with inline rolls yet
+
+VERSION 1.2 CHANGES
+You can now add text at THE END of the roll, and that name will be added to the turn order
+For example:
+/r d20+5 !initiative John
+Will add "John" in the turn order
 */
 on("chat:message", function(msg) {
     if(msg.content.toLowerCase().indexOf("!initiative") !== -1) {
-        var roller = msg.who;
         var msgP = JSON.parse(msg.content);
+        var text = msgP.rolls[2].text;
+        var textSplit = text.split(" ");
+        var roller = (textSplit[textSplit.length - 1].toLowerCase() == "!initiative")?msg.who:textSplit[textSplit.length - 1];
+        log(roller);
         var turnorder = (Campaign().get("turnorder") == "") ? [] : JSON.parse(Campaign().get("turnorder"));
         turnorder.push({
             id: "-1",
